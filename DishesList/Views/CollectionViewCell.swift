870730737f065618870325eefda51f3de87e532a
Nekane Pardo <nekane.pardo@basetis.com>
2021-09-15 16:18:35 +0200

    Created Collection View and Controllers, deleted Table View and Controllers. View Controllers works til SE device

---
+++ DishesList/Views/CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  DishesList
+//
+//  Created by Nekane Pardo on 15/09/2021.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageDishView: UIImageView!
+    @IBOutlet weak var nameDishLabel: UILabel!
+    @IBOutlet weak var priceDishLabel: UILabel!
+}
---
+++ DishesList/Views/CollectionViewController.swift
@@ -0,0 +1,91 @@
+//
+//  CollectionViewController.swift
+//  DishesList
+//
+//  Created by Nekane Pardo on 15/09/2021.
+//
+
+import UIKit
+
+
+class CollectionViewController: UICollectionViewController {
+    private let reuseIdentifier = "Cell"
+    
+    let viewModel = DishViewModel()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Register cell classes
+        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    // MARK: UICollectionViewDataSource
+    // numero de secciones diferentes
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+            return 1 // En este proyecto necesito solo una seccion
+        }
+    // numero de items que mostramos
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return viewModel.getDishesCount()
+    }
+    //
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+       guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Dish", for: indexPath) as? CollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        
+        cell.nameDishLabel.text = viewModel.getNameDish(indexPath.row)
+        cell.priceDishLabel.text = viewModel.getPriceDish(indexPath.row)
+        cell.imageDishView.image = UIImage(named: viewModel.getImageDish(indexPath.row))
+        
+        return cell
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+    
+    }
+    */
+
+}
